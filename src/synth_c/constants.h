27db4c1893773e7fd0ec64cf942671135c8b17a7
Zach Harwood <zharwood314@gmail.com>
2025-03-28 09:13:08 -0600

    Create constants.h

---
+++ src/synth_c/constants.h
@@ -0,0 +1,30 @@
+/*
+Copyright (C) 2025  Zach Harwood
+
+This file is part of SoundForge
+
+SoundForge is a free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#ifndef SYNTHESIS_H
+#define SYNTHESIS_H
+
+extern const int BUFFER_SAMPLES;
+extern const int SAMPLE_RATE;
+extern const int CHANNELS;
+
+extern const int MIDI_TO_A4;
+extern const int MAX_CUTOFF;
+extern const int MAX_RECORDING;
+
+#endif

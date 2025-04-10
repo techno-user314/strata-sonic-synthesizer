215bd082a9356e0c5e456389ddfd36dc395d5d15
Zach Harwood <zharwood314@gmail.com>
2025-04-10 16:33:22 -0600

    Move constants to constants.py

--- src/py_synth.py
+++ src/py_synth.py
@@ -20,7 +20,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import ctypes
 
-VOICES = 12 # Must match constants.h VOICES
+from constants import VOICES
 
 class Envelope(ctypes.Structure):
     _fields_ = [("releasing", ctypes.c_int),

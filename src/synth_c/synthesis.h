1cb4ea0ffd399190c8d21dc4da2fe664d8fa0d32
techno-user314 <zharwood314@gmail.com>
2025-09-20 14:00:50 -0600

    Title change

--- src/synth_c/synthesis.h
+++ src/synth_c/synthesis.h
@@ -1,9 +1,9 @@
 /*
 Copyright (C) 2025  Zach Harwood
 
-This file is part of SoundForge
+This file is part of StrataSonic
 
-SoundForge is a free software: you can redistribute it and/or modify
+StrataSonic is a free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
